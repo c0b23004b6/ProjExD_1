5e4f9e104776a7998f10afe602c6cf319de8f4fc
Asahi <c0b23004b6@edu.teu.ac.jp>
2024-04-16 18:12:49 +0900

    課題１

--- pygame_image.py
+++ pygame_image.py
@@ -5,6 +5,7 @@ import pygame as pg
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
+
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
@@ -20,21 +21,22 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
-        if  key_lst[pg.K_UP]:
+        if key_lst[pg.K_UP]:
             kk_rct.move_ip((0, -1))
         if key_lst[pg.K_DOWN]:
             kk_rct.move_ip((0, +1))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1, 0))
+            kk_rct.move_ip((+2, 0))
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1, 0))
+        kk_rct.move_ip(-1,0)
 
         x=tmr%3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200,0])
         screen.blit(bg_img2, [-x+4800,0])
-        screen.blit(kk_img, [kk_rct-1,kk_rct])
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
